8993a77d62da7f324046d6c380849b4efbfa44d4
Stefan Huber <MSNexploder@gmail.com>
2015-03-22 13:54:17 +0100

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject msgjson "0.0.1"
+(defproject msgjson "0.0.2"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [http-kit "2.1.16"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [http-kit "2.1.19"]
                  [clojure-msgpack "0.1.0-20140117.081453-5"]
                  [clj-json "0.5.3"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.3.2"]]
   :uberjar-name "msgjson-standalone.jar"
-  :min-lein-version "2.3.0")
+  :main msgjson
+  :min-lein-version "2.5.0")
